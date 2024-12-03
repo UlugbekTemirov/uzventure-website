8ef9fd03306be49e39f96c6fa6d5b8b9e1844a2c
UlugbekTemirov <temirovulugbek2003@gmail.com>
2024-12-03 16:42:42 +0500

    changes

---
+++ src/components/Footer.vue
@@ -0,0 +1,33 @@
+<script setup></script>
+
+<template>
+  <footer>
+    <div class="container">
+      <figure class="footer-logo">
+        <img src="/images/logos-logo.png" alt="" />
+      </figure>
+      <ul class="footer-menu d-flex justify-content-center">
+        <li><a href="index.html">HOME</a></li>
+        <li><a href="about-us.html">ABOUT</a></li>
+        <li><a href="tours.html">TOURS</a></li>
+        <li><a href="blog-grid.html">BLOGS</a></li>
+        <li><a href="contact.html">CONTACT</a></li>
+      </ul>
+
+      <div class="footer-social d-flex justify-content-center">
+        <a href="#">
+          <div><i class="fab fa-facebook-f"></i></div>
+        </a>
+        <a href="#">
+          <div><i class="fab fa-twitter"></i></div>
+        </a>
+        <a href="#">
+          <div><i class="fab fa-instagram"></i></div>
+        </a>
+        <a href="#">
+          <div><i class="fab fa-tiktok"></i></div>
+        </a>
+      </div>
+    </div>
+  </footer>
+</template>
